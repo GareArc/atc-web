e5206c62deb4f02ee03f47df0c75d5dcdb180bb8
GareArc <garethcxy.chen@mail.utoronto.ca>
2022-04-10 14:05:05 -0400

    fix merge bug

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,8 +1,6 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-<<<<<<< HEAD
-<<<<<<< HEAD
     service: 'gmail',
     auth: {
         type: 'OAuth2',
@@ -11,23 +9,6 @@ const transporter = nodemailer.createTransport({
         clientSecret: process.env.GMAIL_CLIENT_SECRET,
         refreshToken: process.env.GMAIL_REFRENSH_TOKEN,
         accessToken: process.env.GMAIL_ACCESS_TOKEN
-=======
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: 'atcweb513@gmail.com',
-        pass: 'atc_513666',
->>>>>>> 5b2d9ed... add mailing system
-=======
-    service: 'gmail',
-    auth: {
-        type: 'OAuth2',
-        user: process.env.GMAIL,
-        clientId: process.env.GMAIL_CLIENTID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        refreshToken: process.env.GMAIL_REFRENSH_TOKEN,
-        accessToken: process.env.GMAIL_ACCESS_TOKEN
->>>>>>> 883528f... use OATH2 for gmail
     },
 })
 
