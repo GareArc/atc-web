a2c0c8dd06f663d4f133ee7432669948f01679a6
GareArc <garethcxy.chen@mail.utoronto.ca>
2023-05-07 15:35:49 -0400

    update url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL: process.env.REACT_APP_API_URL,
+    baseURL: "/api",
     headers: {
         "Content-Type": "application/json"
     },
