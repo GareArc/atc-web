8b030bcde66013e694d7f55885cd12b983a4f5d1
GareArc <garethcxy.chen@mail.utoronto.ca>
2023-05-09 03:52:32 -0400

    fix env import issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,22 +2,22 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => { 
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())}  
-  /** @type {import('vite').UserConfig} */
+/** @type {import('vite').UserConfig} */
+export default defineConfig(({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, '..', "VITE_") }
   return defineConfig({
-  
+
     plugins: [react()],
     envDir: '..',
     root: '.',
-    
+
     server: {
       host: true,
-      port: process.env.VITE_PORT,
+      port: env.VITE_PORT,
     },
-  
+
     build: {
       manifest: true,
     },
   })
-}
+})
